ce921be6f52948df716cc68dd073735369ec8ecd
SuspiciousLookingOwl <32597776+SuspiciousLookingOwl@users.noreply.github.com>
2026-08-10 17:09:15 +0800

    use env value and format start script

--- start_example.sh
+++
@@ -1,7 +0,0 @@
-Insurgency/Binaries/Linux/InsurgencyServer-Linux-Shipping \
-    Tell?Scenario=Scenario_Tell_Survival?MaxPlayers=8 \
-    -ModDownloadTravelTo="Precinct?Scenario=Scenario_Precinct_Checkpoint_Security?Mutators=Diff7,NoRestrictedArea,StallCounter,ImprovedAI?LabelOverrideScenario=CHECKPOINT (NORMAL)?MaxPlayers=10" \
-    -MapCycle=MapCycle.txt -Mods -SecurityCode=none -Port=27102 -QueryPort=27131 -log -NoEAC -GameStats \
-    -mutators=Flashlight_Shadows,MapVoteLabels,MoreAmmoPlus,FullyLoaded,AdminCommands,JoinLeaveMessage,PingExt \
-    -GameStatsToken=... -GSLTToken=... \
-    -hostname="[Ten Four] Checkpoint 8vs36 0.8-1.0 (Gun Master, Waves)"
--- start_zombie_example.sh
+++
@@ -1,7 +0,0 @@
-Insurgency/Binaries/Linux/InsurgencyServer-Linux-Shipping \
-    Tell?Scenario=Scenario_Tell_Survival?MaxPlayers=8 \
-    -ModDownloadTravelTo="Precinct?Scenario=Scenario_Precinct_Checkpoint_Security?Lighting=Night?Mutators=StallCounter,ImprovedAI_2,Big,WG3,MoreAmmo,Reloads,Bolts,Quickdraw,ZombiesLite?MaxPlayers=10" \
-    -MapCycle=MapCycleZombie.txt -SecurityCode=none -motd=MotdZombie -Mods -Port=27102 -QueryPort=27131 -log -NoEAC -GameStats \
-    -mutators=Flashlight_Shadows,MapVoteLabels,MoreAmmoPlus,FullyLoaded,AdminCommands,JoinLeaveMessage,PingExt \
-    -GameStatsToken=... -GSLTToken=... \
-    -hostname="[Ten Four] Zombie 8vs96 (Gun Master, Waves)"
