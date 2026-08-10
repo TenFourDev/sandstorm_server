f76fe3523806b927d69e540f9a3b6290039f948c
SuspiciousLookingOwl <32597776+SuspiciousLookingOwl@users.noreply.github.com>
2026-08-10 18:51:51 +0800

    add CapCount mutator

--- start_zombie.sh
+++ start_zombie.sh
@@ -10,6 +10,6 @@ Insurgency/Binaries/Linux/InsurgencyServer-Linux-Shipping \
     Tell?Scenario=Scenario_Tell_Survival?MaxPlayers=10 \
     -ModDownloadTravelTo="Precinct?Scenario=Scenario_Precinct_Checkpoint_Security?Lighting=Night?Mutators=Big?MaxPlayers=10" \
     -MapCycle=MapCycleZombie.txt -SecurityCode=none -motd=MotdZombie -Mods -Port=27102 -QueryPort=27131 -NoEAC -GameStats \
-    -mutators=Flashlight_Shadows,MapVoteLabels,MoreAmmoPlus,FullyLoaded,AdminCommands,JoinLeaveMessage,PingExt,StallCounter,ImprovedAI_2,WG3,MoreAmmo,Reloads,Bolts,Quickdraw,ZombiesLite \
+    -mutators=Flashlight_Shadows,MapVoteLabels,MoreAmmoPlus,FullyLoaded,AdminCommands,JoinLeaveMessage,PingExt,StallCounter,ImprovedAI_2,WG3,MoreAmmo,Reloads,Bolts,Quickdraw,ZombiesLite,CapCount \
     -GameStatsToken="${GAME_STATS_TOKEN}" -GSLTToken="${GSLT_TOKEN}" \
     -hostname="[Ten Four] Zombie 8vs96 (Gun Master, Waves)"
