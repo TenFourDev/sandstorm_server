4e3528c9f627c3221eec2bfaea042a3a4c346869
SuspiciousLookingOwl <32597776+SuspiciousLookingOwl@users.noreply.github.com>
2026-08-10 23:18:12 +0800

    remove duplicate more ammo mutator

--- start_zombie.sh
+++ start_zombie.sh
@@ -12,6 +12,6 @@ Insurgency/Binaries/Linux/InsurgencyServer-Linux-Shipping \
     Tell?Scenario=Scenario_Tell_Survival?MaxPlayers=6 \
     -ModDownloadTravelTo="Precinct?Scenario=Scenario_Precinct_Checkpoint_Security?Lighting=Night?Mutators=Big?MaxPlayers=6" \
     -MapCycle=MapCycleZombie.txt -SecurityCode=none -motd=MotdZombie -Mods -Port=27102 -QueryPort=27131 -NoEAC -GameStats \
-    -mutators=Flashlight_Shadows,MapVoteLabels,MoreAmmoPlus,FullyLoaded,AdminCommands,JoinLeaveMessage,PingExt,StallCounter,ImprovedAI_2,WG3,MoreAmmo,Reloads,Bolts,Quickdraw,ZombiesLite,CapCount,Healthy \
+    -mutators=Flashlight_Shadows,MapVoteLabels,MoreAmmoPlus,FullyLoaded,AdminCommands,JoinLeaveMessage,PingExt,StallCounter,ImprovedAI_2,WG3,Reloads,Bolts,Quickdraw,ZombiesLite,CapCount,Healthy \
     -GameStatsToken="${GAME_STATS_TOKEN}" -GSLTToken="${GSLT_TOKEN}" \
     -hostname="[Ten Four] Zombie 6vs96 (Gun Master, Waves)"
