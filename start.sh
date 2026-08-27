27f54444e2d0a39896e7a41c4d7ec11ae2c06b85
SuspiciousLookingOwl <vinjongm@gmail.com>
2026-08-27 04:14:29 +0000

    checkpoint balancing

--- start.sh
+++ start.sh
@@ -13,6 +13,6 @@ Insurgency/Binaries/Linux/InsurgencyServer-Linux-Shipping \
     Tell?Scenario=Scenario_Tell_Survival?MaxPlayers=10 \
     -ModDownloadTravelTo="Precinct?Scenario=Scenario_Precinct_Checkpoint_Security?Mutators=Diff7?MaxPlayers=10" \
     -MapCycle=MapCycle.txt -Mods -SecurityCode=none -Port=27102 -QueryPort=27131 -NoEAC -GameStats \
-    -mutators=Flashlight_Shadows,MapVoteLabels,MoreAmmoPlus,FullyLoaded,AdminCommands,JoinLeaveMessage,PingExt,NoRestrictedArea,StallCounter,ImprovedAI,CapCount,Reloads,Bolts,Canoeing \
+    -mutators=Flashlight_Shadows,MapVoteLabels,MoreAmmoPlus,FullyLoaded,AdminCommands,JoinLeaveMessage,PingExt,NoRestrictedArea,StallCounter,ImprovedAI,CapCount,Reloads,Bolts,Canoeing,AwardWaves \
     -GameStatsToken="${GAME_STATS_TOKEN}" -GSLTToken="${GSLT_TOKEN}" \
     -hostname="[Ten Four] Checkpoint 8vs36 0.8-1.0 (Gun Master, Waves)"
