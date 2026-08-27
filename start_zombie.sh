22ffd88d97ec635ab8d0d2a97b1559a293eb8d23
SuspiciousLookingOwl <vinjongm@gmail.com>
2026-08-27 04:20:10 +0000

    zombie balancing

--- start_zombie.sh
+++ start_zombie.sh
@@ -10,9 +10,9 @@ cp ./Insurgency/Saved/Config/LinuxServer/GameZombie.ini ./Insurgency/Saved/Confi
 cp ./Insurgency/Saved/Config/LinuxServer/EngineZombie.ini ./Insurgency/Saved/Config/LinuxServer/Engine.ini
 
 Insurgency/Binaries/Linux/InsurgencyServer-Linux-Shipping \
-    Tell?Scenario=Scenario_Tell_Survival?MaxPlayers=6 \
-    -ModDownloadTravelTo="Precinct?Scenario=Scenario_Precinct_Checkpoint_Security?Lighting=Night?Mutators=Big?MaxPlayers=6" \
+    Tell?Scenario=Scenario_Tell_Survival?MaxPlayers=5 \
+    -ModDownloadTravelTo="Precinct?Scenario=Scenario_Precinct_Checkpoint_Security?Lighting=Night?Mutators=Big,Healthy?MaxPlayers=5" \
     -MapCycle=MapCycleZombie.txt -SecurityCode=none -motd=MotdZombie -Mods -Port=27102 -QueryPort=27131 -NoEAC -GameStats \
-    -mutators=Flashlight_Shadows,MapVoteLabels,MoreAmmoPlus,FullyLoaded,AdminCommands,JoinLeaveMessage,PingExt,StallCounter,ImprovedAI_2,Reloads,Bolts,Quickdraw,ZombiesLite,CapCount,Canoeing \
+    -mutators=Flashlight_Shadows,MapVoteLabels,MoreAmmoPlus,MoreAmmo,FullyLoaded,AdminCommands,JoinLeaveMessage,PingExt,StallCounter,ImprovedAI_2,Reloads,Bolts,Quickdraw,ZombiesLite,CapCount,Canoeing,AwardWaves \
     -GameStatsToken="${GAME_STATS_TOKEN}" -GSLTToken="${GSLT_TOKEN}" \
-    -hostname="[Ten Four] Zombie 6vs96 (Gun Master, Waves)"
+    -hostname="[Ten Four] Zombie 4vs80 (Gun Master, Waves)"
