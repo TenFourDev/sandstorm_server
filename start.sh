44bd5f2bff76787f641943ac7ec32a9a053979e0
SuspiciousLookingOwl <vinjongm@gmail.com>
2026-09-02 06:31:16 +0000

    fix travel mutators

--- start.sh
+++ start.sh
@@ -10,7 +10,7 @@ cp ./Insurgency/Saved/Config/LinuxServer/GameCheckpoint.ini ./Insurgency/Saved/C
 cp ./Insurgency/Saved/Config/LinuxServer/EngineCheckpoint.ini ./Insurgency/Saved/Config/LinuxServer/Engine.ini
 
 Insurgency/Binaries/Linux/InsurgencyServer-Linux-Shipping \
-    Tell?Scenario=Scenario_Tell_Survival?MaxPlayers=10 \
+    Tell?Scenario=Scenario_Tell_Survival?Mutators=Diff7?MaxPlayers=10 \
     -ModDownloadTravelTo="Precinct?Scenario=Scenario_Precinct_Checkpoint_Security?Mutators=Diff7?MaxPlayers=10" \
     -MapCycle=MapCycle.txt -Mods -SecurityCode=none -Port=27102 -QueryPort=27131 -NoEAC -GameStats \
     -mutators=Flashlight_Shadows,MapVoteLabels,AdminCommands,JoinLeaveMessage,PingExt,NoRestrictedArea,StallCounter,ImprovedAI,CapCount,Reloads,Bolts,Canoeing,AwardWaves,EnemyBoatSpotted_OldFire,EBSTechnicals \
