91795af320577e2df775d7042c9b7dddc8644345
SuspiciousLookingOwl <vinjongm@gmail.com>
2026-09-03 13:07:46 +0000

    fix travel with mutators

--- start_zombie.sh
+++ start_zombie.sh
@@ -10,9 +10,9 @@ cp ./Insurgency/Saved/Config/LinuxServer/_Game.ini ./Insurgency/Saved/Config/Lin
 cp ./Insurgency/Saved/Config/LinuxServer/_Engine.ini ./Insurgency/Saved/Config/LinuxServer/Engine.ini
 
 Insurgency/Binaries/Linux/InsurgencyServer-Linux-Shipping \
-    Tell?Scenario=Scenario_Tell_Survival?MaxPlayers=5?Mutators=Mid,Healthy \
-    -ModDownloadTravelTo="Precinct?Scenario=Scenario_Precinct_Checkpoint_Security?MaxPlayers=5?Mutators=Mid,Healthy?Lighting=Night" \
-    -MapCycle=MapCycleZombie.txt -SecurityCode=none -motd=MotdZombie -Mods -Port=27103 -QueryPort=27132 -NoEAC -GameStats \
+    Tell?Scenario=Scenario_Tell_Survival?Lighting=Night?MaxPlayers=5 \
+    -ModDownloadTravelTo="Precinct?Scenario=Scenario_Precinct_Checkpoint_Security?Lighting=Night?MaxPlayers=5?Mutators=Mid" \
+    -MapCycle=MapCycleZombie.txt -SecurityCode=none -motd=MotdZombie -Mods -Port=27103 -QueryPort=27132 -NoEAC -GameStats \ 
     -mutators=Flashlight_Shadows,MapVoteLabels,MoreAmmoPlus,MoreAmmo,FullyLoaded,AdminCommands,JoinLeaveMessage,PingExt,StallCounter,ImprovedAI_2,Reloads,Bolts,Quickdraw,ZombiesLite,CapCount,Canoeing,AwardWaves \
     -GameStatsToken="${GAME_STATS_TOKEN}" -GSLTToken="${GSLT_TOKEN_ZOMBIE}" \
     -hostname="[Ten Four] Zombie 4vs40 (Gun Master, Waves)"
