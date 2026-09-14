0ffbe07083b332ad62c45cd40ea776e9e55dc51c
SuspiciousLookingOwl <vinjongm@gmail.com>
2026-09-14 15:15:43 +0000

    zombie balancing

--- start_zombie.sh
+++ start_zombie.sh
@@ -9,10 +9,9 @@ fi
 cp ./Insurgency/Saved/Config/LinuxServer/_Game.ini ./Insurgency/Saved/Config/LinuxServer/Game.ini
 cp ./Insurgency/Saved/Config/LinuxServer/_Engine.ini ./Insurgency/Saved/Config/LinuxServer/Engine.ini
 
-
-MOD_DOWNLOAD_TRAVEL_TO="$(awk -f ./Insurgency/Config/Server/random_map.awk -v acceptmutator=Mid -v maxplayers=5 -v mapfile=./Insurgency/Config/Server/ScenarioMap.txt ./Insurgency/Config/Server/MapCycleZombie.txt 2>/dev/null)"
+MOD_DOWNLOAD_TRAVEL_TO="$(awk -f ./Insurgency/Config/Server/random_map.awk -v maxplayers=5 -v mapfile=./Insurgency/Config/Server/ScenarioMap.txt ./Insurgency/Config/Server/MapCycleZombie.txt 2>/dev/null)"
 if [ -z "$MOD_DOWNLOAD_TRAVEL_TO" ]; then
-    MOD_DOWNLOAD_TRAVEL_TO="Precinct?Scenario=Scenario_Precinct_Checkpoint_Security?Lighting=Night?MaxPlayers=5?Mutators=Healthy"
+    MOD_DOWNLOAD_TRAVEL_TO="Precinct?Scenario=Scenario_Precinct_Checkpoint_Security?Lighting=Night?MaxPlayers=5?Mutators=Mid"
 fi
 
 echo "==> Random starting map: $MOD_DOWNLOAD_TRAVEL_TO"
@@ -21,6 +20,6 @@ Insurgency/Binaries/Linux/InsurgencyServer-Linux-Shipping \
     Tell?Scenario=Scenario_Tell_Survival?Lighting=Night?MaxPlayers=5 \
     -ModDownloadTravelTo="$MOD_DOWNLOAD_TRAVEL_TO" \
     -MapCycle=MapCycleZombie.txt -SecurityCode=none -motd=MotdZombie -Mods -Port=27103 -QueryPort=27132 -NoEAC -GameStats \
-    -mutators=Flashlight_Shadows,MapVoteLabels,MoreAmmoPlus,MoreAmmo,FullyLoaded,AdminCommands,JoinLeaveMessage,PingExt,StallCounter,ImprovedAI_2,Reloads,Bolts,Quickdraw,ZombiesLite,CapCount,Canoeing,AwardWaves \
+    -mutators=Flashlight_Shadows,MapVoteLabels,MoreAmmoPlus,MoreAmmo,FullyLoaded,AdminCommands,JoinLeaveMessage,PingExt,StallCounter,ImprovedAI_2,Reloads,Bolts,Quickdraw,ZombiesLite,CapCount,Canoeing,AwardWaves,Healthy \
     -GameStatsToken="${GAME_STATS_TOKEN}" -GSLTToken="${GSLT_TOKEN_ZOMBIE}" \
     -hostname="[Ten Four] Zombie 4vs56 (Gun Master, Waves)"
--- start_zombie_frenzy.sh
+++ start_zombie_frenzy.sh
@@ -9,9 +9,9 @@ fi
 cp ./Insurgency/Saved/Config/LinuxServer/_Game.ini ./Insurgency/Saved/Config/LinuxServer/Game.ini
 cp ./Insurgency/Saved/Config/LinuxServer/_Engine.ini ./Insurgency/Saved/Config/LinuxServer/Engine.ini
 
-MOD_DOWNLOAD_TRAVEL_TO="$(awk -f ./Insurgency/Config/Server/random_map.awk -v acceptmutator=Mid -v maxplayers=5 -v mapfile=./Insurgency/Config/Server/ScenarioMap.txt ./Insurgency/Config/Server/MapCycleZombie.txt 2>/dev/null)"
+MOD_DOWNLOAD_TRAVEL_TO="$(awk -f ./Insurgency/Config/Server/random_map.awk -v maxplayers=5 -v mapfile=./Insurgency/Config/Server/ScenarioMap.txt ./Insurgency/Config/Server/MapCycleZombie.txt 2>/dev/null)"
 if [ -z "$MOD_DOWNLOAD_TRAVEL_TO" ]; then
-    MOD_DOWNLOAD_TRAVEL_TO="Precinct?Scenario=Scenario_Precinct_Checkpoint_Security?Lighting=Night?MaxPlayers=5?Mutators=Healthy"
+    MOD_DOWNLOAD_TRAVEL_TO="Precinct?Scenario=Scenario_Precinct_Checkpoint_Security?Lighting=Night?MaxPlayers=5?Mutators=Mid"
 fi
 
 echo "==> Random starting map: $MOD_DOWNLOAD_TRAVEL_TO"
@@ -20,6 +20,6 @@ Insurgency/Binaries/Linux/InsurgencyServer-Linux-Shipping \
     Tell?Scenario=Scenario_Tell_Survival?Lighting=Night?MaxPlayers=5 \
     -ModDownloadTravelTo="$MOD_DOWNLOAD_TRAVEL_TO" \
     -MapCycle=MapCycleZombie.txt -SecurityCode=none -motd=MotdZombieFrenzy -Mods -Port=27104 -QueryPort=27133 -NoEAC -GameStats \
-    -mutators=Flashlight_Shadows,MapVoteLabels,MoreAmmoPlus,MoreAmmo,FullyLoaded,AdminCommands,JoinLeaveMessage,PingExt,StallCounter,ImprovedAI_2,Reloads,Bolts,Quickdraw,ZombiesLite,CapCount,Canoeing,AwardWaves \
+    -mutators=Flashlight_Shadows,MapVoteLabels,MoreAmmoPlus,MoreAmmo,FullyLoaded,AdminCommands,JoinLeaveMessage,PingExt,StallCounter,ImprovedAI_2,Reloads,Bolts,Quickdraw,ZombiesLite,CapCount,Canoeing,AwardWaves,Healthy \
     -GameStatsToken="${GAME_STATS_TOKEN}" -GSLTToken="${GSLT_TOKEN_ZOMBIE_FRENZY}" \
     -hostname="[Ten Four] Zombie Frenzy 4vs84 (Gun Master, Waves)"
